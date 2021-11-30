42041d976e0f9f8969139bd9bb2c7b552b25545e
Spectrox <67061496+Spectroxx@users.noreply.github.com>
2021-11-29 21:13:04 -0500

    1.01

--- scripts/tampermonkey-sportshd-controls.js
+++ scripts/tampermonkey-sportshd-controls.js
@@ -1,15 +1,17 @@
 // ==UserScript==
 // @name         LiveStream Video Controls
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.01
 // @description  Auto Enables Video Controls on SportsHD.live
 // @author       Spectrox
 // @match        https://live.cdnz.one/*
 // @icon         https://www.google.com/s2/favicons?domain=cdnz.one
 // @grant        none
+// @homepageURL  https://github.com/Spectroxx/tampermonkey-scripts/tree/main/scripts
+// @downloadURL  https://raw.githubusercontent.com/Spectroxx/tampermonkey-scripts/main/scripts/tampermonkey-sportshd-controls.js
 // ==/UserScript==
 
 window.addEventListener("load", function(event) {
     cbut();
-    console.log("Enabled Stream Controls.");
+    console.log("Enabled Video Controls.");
 });
