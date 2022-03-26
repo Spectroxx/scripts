bfc3f680a8453dcfb0391ebf4f2cd130dd1bb0ae
Spectrox <67061496+Spectroxx@users.noreply.github.com>
2022-03-26 14:02:45 -0400

    0.1

--- userscripts/terraria-wiki-cleaner.js
+++ userscripts/terraria-wiki-cleaner.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name        Terraria Wiki Cleaner
+// @description Terraria Wiki Cleaner removes the annoying header, sidebar and footer banners from the wiki while also fixing the spacing issues when removing those ads.
 // @match       *://terraria.wiki.gg/*
 // @author      Spectrox
 // @version     0.1
@@ -8,14 +9,53 @@
 // @downloadURL https://raw.githubusercontent.com/Spectroxx/scripts/main/userscripts/terraria-wiki-cleaner.js
 // @icon        https://external-content.duckduckgo.com/ip3/terraria.wiki.gg.ico
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js
-// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant       GM_addStyle
 // ==/UserScript==
 
-waitForKeyElements (".games-showcase-sidebar", killNode);
-waitForKeyElements (".header-showcase", killNode);
-waitForKeyElements (".games-showcase-footer", killNode);
+function waitForKeyElements(selectorTxt, actionFunction, bWaitOnce, iframeSelector) {
+    var targetNodes, btargetsFound;
 
-function killNode (jNode) {
-    jNode.remove ();
+    if (typeof iframeSelector == "undefined") targetNodes = $(selectorTxt);
+    else targetNodes = $(iframeSelector).contents().find(selectorTxt);
+
+    if (targetNodes && targetNodes.length > 0) {
+        btargetsFound = true;
+        targetNodes.each(function () {
+            var jThis = $(this);
+            var alreadyFound = jThis.data("alreadyFound") || false;
+
+            if (!alreadyFound) {
+                var cancelFound = actionFunction(jThis);
+                if (cancelFound) btargetsFound = false;
+                else jThis.data("alreadyFound", true);
+            }
+        });
+    } else {
+        btargetsFound = false;
+    }
+  
+    var controlObj = waitForKeyElements.controlObj || {};
+    var controlKey = selectorTxt.replace(/[^\w]/g, "_");
+    var timeControl = controlObj[controlKey];
+
+    if (btargetsFound && bWaitOnce && timeControl) {
+        clearInterval(timeControl);
+        delete controlObj[controlKey];
+    } else {
+        if (!timeControl) {
+            timeControl = setInterval(function () {
+                waitForKeyElements(selectorTxt, actionFunction, bWaitOnce, iframeSelector);
+            }, 300);
+            controlObj[controlKey] = timeControl;
+        }
+    }
+    waitForKeyElements.controlObj = controlObj;
+}
+
+waitForKeyElements(".games-showcase-sidebar", killNode);
+waitForKeyElements(".header-showcase", killNode);
+waitForKeyElements(".games-showcase-footer", killNode);
+
+function killNode(jNode) {
+    jNode.remove();
 }
