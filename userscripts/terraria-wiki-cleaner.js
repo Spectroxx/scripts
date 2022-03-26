aed67c90aadcbadaf7f7aa8b7c556240eb45fe78
Spectrox <67061496+Spectroxx@users.noreply.github.com>
2022-03-26 13:45:53 -0400

    0.1

--- userscripts/terraria-wiki-cleaner.js
+++ userscripts/terraria-wiki-cleaner.js
@@ -3,6 +3,8 @@
 // @match       *://terraria.wiki.gg/*
 // @author      Spectrox
 // @version     0.1
+// @license     http://unlicense.org
+// @namespace   *://terraria.wiki.gg/*
 // @downloadURL https://raw.githubusercontent.com/Spectroxx/scripts/main/userscripts/terraria-wiki-cleaner.js
 // @icon        https://external-content.duckduckgo.com/ip3/terraria.wiki.gg.ico
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js
